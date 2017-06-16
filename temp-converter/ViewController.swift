ff085a6a81a86fc5472e83ba6679cd7d955a685d
Christina Glezil <christina.glezil@gmail.com>
2017-06-16 18:04:36 -0400

    Initial Commit

---
+++ temp-converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  temp-converter
+//
+//  Created by Christina Glezil on 6/16/17.
+//  Copyright © 2017 Christina Glezil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
