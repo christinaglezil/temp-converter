aa022a5eb3722312c600fe4158939001bdfc1e35
Christina Glezil <christina.glezil@gmail.com>
2017-06-17 20:42:22 -0400

    temperature-converter
    iOS temp converter

--- temp-converter/ViewController.swift
+++ temp-converter/ViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    
+    @IBOutlet weak var conversionLabel: UILabel!
+    @IBOutlet weak var tempEntry: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    func updateCountByOne (){
+        count += 1
+        conversionLabel.text = String(count) + " conversions"
+    }
+    
+    
+    @IBAction func convertClicked(_ sender: Any) {
+        
+        if let result = tempEntry.text{
+            if(result == ""){
+                return
+            }
+            else{
+                if let num = Double(result){
+                   let output = num * (9/5)+32
+                    resultLabel.text = String(output)
+                    
+                    updateCountByOne()
+            }
+        }
+    }
+        
+    func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+   }
 }
-
